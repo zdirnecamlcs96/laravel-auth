fdea49b26ff3b937cb5424ff4e9f3d7d8c9aa81f
Chia Sheng <chiasheng@etctech.com.my>
2022-06-20 12:55:06 +0800

    1) Added "laravel/ui" as dependency 2) Added Forget Password and Reset Password APIs 3) Fixed PSR-4 rename filename from "HasThirdParty" to "HasSocialIdentity"

--- routes/authentication.php
+++ routes/authentication.php
@@ -5,7 +5,9 @@ use Zdirnecamlcs96\Auth\Contracts\ShouldAuthenticate;
 use Zdirnecamlcs96\Auth\Http\Controllers\{
     LoginController,
     AccountController,
-    RegisterController
+    RegisterController,
+    ForgotPasswordController,
+    ResetPasswordController
 };
 
 foreach (config('auth.guards') as $guard => $settings) {
@@ -16,19 +18,20 @@ foreach (config('auth.guards') as $guard => $settings) {
 
         Route::match(['get', 'post'], 'third-party-login/{provider}/callback', [LoginController::class, 'thirdPartyLoginCallback'])->name('third-party-login.callback');
 
-        Route::group(['middleware' => ["guest:{$guard}"]], function() {
+        Route::group(['as' => "{$guard}." , 'middleware' => ["guest:{$guard}"]], function() {
             Route::controller(LoginController::class)->group(function() {
                 Route::post('login', 'login')->name('login');
                 Route::post('third-party-login', 'thirdPartyLogin')->name('third-party-login');
             });
             Route::post('register', [RegisterController::class, 'register']);
-            // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-            //             ->name('password.email');
-            // Route::post('reset-password', [NewPasswordController::class, 'store'])
-            //             ->name('password.update');
+            Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+            Route::controller(ResetPasswordController::class)->group(function() {
+                Route::get('reset-password/{token}', 'showResetForm')->name('password.reset');
+                Route::post('reset-password', 'reset')->name('password.update');
+            });
         });
 
-        Route::group(['middleware' => ["auth:{$guard}"]], function() {
+        Route::group(['as' => "{$guard}.", 'middleware' => ["auth:{$guard}"]], function() {
             Route::post('logout', [LoginController::class, "logout"])->name('logout');
 
             Route::group(["prefix" => "account", "as" => "account", "controller" => AccountController::class], function() {
