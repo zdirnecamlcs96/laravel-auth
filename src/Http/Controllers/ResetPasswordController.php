c8d3b0554eb3aed78c264b02a4eb207456e8e5f3
Chia Sheng <chiasheng@etctech.com.my>
2022-07-13 23:06:20 +0800

    Reset Password update

--- src/Http/Controllers/ResetPasswordController.php
+++ src/Http/Controllers/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace Zdirnecamlcs96\Auth\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,49 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request)
+    {
+        $token = $request->route()->parameter('token');
+
+        $redirectTo = config('authentication.reset_password.password_reset_redirect');
+
+        $data = [
+            'token' => $token,
+            'email' => $request->email
+        ];
+
+        // Redirect to custom url, mainly for SPA web application
+        if ($redirectTo)
+        {
+            return redirect("{$redirectTo}?" . http_build_query($data));
+        }
+
+        return view('auth.passwords.reset')->with($data);
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        if ($request->wantsJson()) {
+            return $this->__apiSuccess(__($response));
+        }
+
+        return redirect($this->redirectPath())
+                            ->with('status', __($response));
+    }
 }
